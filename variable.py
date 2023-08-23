30c71f4803e5d71b31dfd416a8e3d30e0504b166
ujh1008 <142972359+ujh1008@users.noreply.github.com>
2023-08-23 16:40:22 +0900

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,22 @@
+P = 3.14
+R = float(input())
+a = P * R * 2
+b = P * R ** 2
+print(a,b)
+
+P=int(input())
+Q=float(input())
+R=int(input())
+a = P - P * (Q/100) + R
+print(a)
+
+a = int(input())
+b = int(input())
+c = a-b
+print(c)
+
+year = int(input())
+month = int(input())
+day = int(input())
+
+print('%d-%02d-%02d' % (year, month, day))
